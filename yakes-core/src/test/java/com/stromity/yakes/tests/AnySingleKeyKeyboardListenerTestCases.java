0370a172c27eb20d34e8302b6d5137217a0e42f3
Mat <29580002+LilMat@users.noreply.github.com>
2021-06-04 23:28:56 +0200

    Refactoring

--- yakes-core/src/test/java/com/stromity/yakes/tests/AnySingleKeyKeyboardListenerTestCases.java
+++ yakes-core/src/test/java/com/stromity/yakes/tests/AnySingleKeyKeyboardListenerTestCases.java
@@ -3,10 +3,12 @@ package com.stromity.yakes.tests;
 import com.stromity.yakes.KeyboardEventSystem;
 import com.stromity.yakes.PressType;
 import com.stromity.yakes.listener.AnySingleKeyKeyboardListener;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AnySingleKeyKeyboardListenerTestCases {
 
@@ -15,9 +17,18 @@ public class AnySingleKeyKeyboardListenerTestCases {
         KeyboardEventSystem eventSystem = new KeyboardEventSystem(new HashMap<>());
         AnySingleKeyKeyboardListener keyKeyboardListener = new AnySingleKeyKeyboardListener(new HashMap<>(), PressType.ON_PRESS);
 
+        AtomicReference<String> result = new AtomicReference<>();
+
         eventSystem.registerListener("ANY_TEST", keyKeyboardListener);
 
-        keyKeyboardListener.bind("R", () -> System.out::println);
-        eventSystem.fireEvent(Collections.singletonList("R"), PressType.ON_PRESS);
+        // Faking key press.
+        String pressedKey = "R";
+
+        keyKeyboardListener.bind(pressedKey, () -> result::set);
+
+        // Call the fake key press.
+        eventSystem.fireEvent(Collections.singletonList(pressedKey), PressType.ON_PRESS);
+
+        Assert.assertEquals(pressedKey, result.get());
     }
 }
---
+++ yakes-lwjgl3/src/main/java/com/stromity/yakes_lwjgl3/KeyboardEventSystemLWJGL3.java
@@ -0,0 +1,5 @@
+package com.stromity.yakes_lwjgl3;
+
+public class KeyboardEventSystemLWJGL3 {
+
+}
